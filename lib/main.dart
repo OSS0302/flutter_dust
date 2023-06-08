aecef87032f45a4d001bb6d8337131f9cf9bcdc5
ohsangsoo <tkdtn3264@gmail.com>
2023-06-08 16:53:22 +0900

    현재위치 미세먼지 간격16으로  띄우기

--- lib/main.dart
+++ lib/main.dart
@@ -28,63 +28,69 @@ class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          Text(
-              '현재 위치 미세먼지',
-            style: TextStyle(fontSize: 30),
-          ),
-          Card(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget> [
-                      Text('지역'),
-                      Text('80',style: TextStyle(fontSize: 40),),
-                      Text('보통',style: TextStyle(fontSize: 20),),
-                    ],
-                  ),
-                  color: Colors.yellow,
-                ),
-              Row(
-                mainAxisAlignment:  MainAxisAlignment.spaceAround,
-                  children: <Widget> [
-                    Row(
-                      children: [
-                        Text('이미지'),
-                        SizedBox(
-                          width: 16,
-                        ),//여백주기 16
-                        Text('11°',style:  TextStyle(fontSize: 16),),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            Text(
+                '현재 위치 미세먼지',
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget> [
+                        Text('지역'),
+                        Text('80',style: TextStyle(fontSize: 40),),
+                        Text('보통',style: TextStyle(fontSize: 20),),
                       ],
                     ),
-                    Text('습도 100%'),
-                    Text('풍속 5.7m/s'),
-            ],
-          )
-            ],),
-          ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(30),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 50.0),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.orange)
-                ),
-                onPressed: () {},
-                child: Icon(
-                  Icons.refresh,
-                  color: Colors.white,
+                    color: Colors.yellow,
+                  ),
+                Row(
+                  mainAxisAlignment:  MainAxisAlignment.spaceAround,
+                    children: <Widget> [
+                      Row(
+                        children: [
+                          Text('이미지'),
+                          SizedBox(
+                            width: 16,
+                          ),//여백주기 16
+                          Text('11°',style:  TextStyle(fontSize: 16),),
+                        ],
+                      ),
+                      Text('습도 100%'),
+                      Text('풍속 5.7m/s'),
+              ],
+            )
+              ],),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 50.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.orange)
+                  ),
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.refresh,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          )
-        ],),
+            )
+          ],),
+        ),
       ),
     );
   }
