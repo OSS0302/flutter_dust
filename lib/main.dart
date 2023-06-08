7fc34f7a8899fff95b83fecbd803198ae5bacc1f
ohsangsoo <tkdtn3264@gmail.com>
2023-06-08 15:41:01 +0900

    글자의 간격이 생긴다.

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class _MainState extends State<Main> {
             child: Column(
               children: <Widget>[
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget> [
                     Text('지역'),
                     Text('80'),
@@ -47,6 +48,7 @@ class _MainState extends State<Main> {
                   ],
                 ),
               Row(
+                mainAxisAlignment:  MainAxisAlignment.spaceAround,
                   children: <Widget> [
                     Text('지역'),
                     Text('80'),
