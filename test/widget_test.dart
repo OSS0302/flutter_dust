a1ecb32399155e613ce56e4801ce5fd0a987edb9
ohsangsoo <tkdtn3264@gmail.com>
2023-06-09 16:32:16 +0900

    비동기로 작동하기 때문에 await추가하고 async를 추가한다.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_dust/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  test('http 통신 테스트',() async {
+   var response = await http.get('https://api.airvisual.com/v2/nearest_city?key=8a092729-a723-4c7e-befa-50e6921a48fb' as Uri);
+   });
 }
