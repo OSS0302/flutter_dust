e40ff47b90af2762caf54e34c70b7ac33f1e7572
ohsangsoo <tkdtn3264@gmail.com>
2023-06-10 20:18:29 +0900

    습도 값 넣어주기

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class _MainState extends State<Main> {
                               style:  TextStyle(fontSize: 16),),
                           ],
                         ),
-                        Text('습도 100%'),
+                        Text('습도 ${_result.data?.current?.weather?.hu}'),
                         Text('풍속 5.7m/s'),
               ],
             ),
