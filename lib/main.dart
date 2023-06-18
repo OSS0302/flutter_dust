dd29f2b0ecdea1b20c8b0f3b114530f07aa4e3e4
ohsangsoo <tkdtn3264@gmail.com>
2023-06-18 21:48:17 +0900

    로직 분리하기

--- lib/main.dart
+++ lib/main.dart
@@ -31,22 +31,6 @@ class _MainState extends State<Main> {
   // air 결과 값 가져오기
   AirResult? _result;
 
-  // 비동기로 데이터를 얻어서 가져온다.
-  Future<dynamic> fetchData() async {
-    var toUri = Uri.parse(
-        'http://api.airvisual.com/v2/nearest_city?key=8a092729-a723-4c7e-befa-50e6921a48fb');
-    var response = await http.get(toUri);
-
-    // 공기 결과
-    AirResult result = AirResult.fromJson(json.decode(response.body));
-
-    if(result.data != null){
-      print(result.data!.current.toString());
-      return result;
-    } else {
-      return null;
-    }
-  }
 
   @override
   void initState() {
